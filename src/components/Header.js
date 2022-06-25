91cd9942629ba1ac95a6342ec889911c5bd3a73d
Gabriele Concas <gabrieleconcas96@gmail.com>
2022-06-25 16:05:20 +0200

    setup completed

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const Header = () => {
+    return <Nav>Header</Nav>;
+};
+
+export default Header;
+
+const Nav = styled.nav``;
